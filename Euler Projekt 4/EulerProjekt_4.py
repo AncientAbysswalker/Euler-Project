52703145c72a5760095bcba4c35ea83c3326c98b
AncientAbysswalker <ancientabysswalker@gmail.com>
2017-07-09 14:40:41 -0700

    Largest palindrome product

--- Euler Projekt 4/EulerProjekt_4.py
+++ Euler Projekt 4/EulerProjekt_4.py
@@ -17,23 +17,25 @@ def isPalindrome(number):
 #Problem definition
 maxMult=999
 maxPalindrome=0
+multiples=[0,0]
 
 
 #Check largest products for first palindrome
 for i in reversed(range(80,maxMult+1)):
     #Centric Diagonal
 	if (i)*(i)<maxPalindrome:
-		print(maxPalindrome)
+		print(maxPalindrome,multiples)
 		break
 	for j in range(0,maxMult-i+1):
 		if isPalindrome((i+j)*(i-j)):
 			maxPalindrome=(i+j)*(i-j)
+			multiples=[(i+j),(i-j)]
 		
 	#Off-Centric Diagonal
 	if (i)*(i-1)<maxPalindrome:
-		print(maxPalindrome)
+		print(maxPalindrome,multiples)
 		break
 	for j in range(0,maxMult-i+1):
 		if isPalindrome((i+j)*(i-j-1)):
 			maxPalindrome=(i+j)*(i-j-1)
-            
+			multiples=[(i+j),(i-j-1)]
