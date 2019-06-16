06bcfe258febfd201ab7f44bcf3f41c8f59d4042
Ancient Abysswalker <ancientabysswalker@gmail.com>
2019-06-16 14:35:10 -0700

    Unit tests and functionality integration for permute and increment functions

--- Euler Projekt 047 - Distinct prime factors/EulerProjekt_47.py
+++ Euler Projekt 047 - Distinct prime factors/EulerProjekt_47.py
@@ -33,41 +33,92 @@ def load_primes():
     return primes
 
 
-def permute_factors(factors):
+def permute_factor_sets(factor_sets):
     """Generate permutations of the progressions of factors"""
-    if factors == None:
+
+    # Handling for nothing passed
+    if factor_sets is None:
         return None
 
-    if type(factors) == int:
-        return factors + 1
+    # Handling for only one integer passed
+    if type(factor_sets) == int:
+        return factor_sets + 1
 
-    if type(factors) == list:
-        if factors == [0, 0, 0, 0]:
-            return [0, 0, 0, 1]
+    # Intended pathway for function to take
+    if type(factor_sets) == list:
+        if all(isinstance(x, int) for x in factor_sets):
+            return increment_factors(factor_sets)
+        elif all(isinstance(x, list) for x in factor_sets):
+            new_factor_sets = []
+            for factors in factor_sets:
+                _temp = increment_factors(factors)
+                for new_factors in _temp:
+                    if new_factors not in new_factor_sets:
+                        new_factor_sets.append(new_factors)
+                print(new_factor_sets)
+            return sorted(new_factor_sets)
 
     raise TypeError("permute_factors(n) only functions for type(n) as None, int, or list: int")
 
 
-class TestPermute(unittest.TestCase):
-    def test_none(self):
-        """Test behaviour if nothing is passed"""
-        self.assertIsNone(permute_factors(None))
+def increment_factors(factors):
+    """Increment factors and remove duplicates"""
+    identity_matrix = []
+    results = []
+    size = len(factors)
 
-    def test_integer(self):
-        """Test behaviour if a single integer is passed"""
-        self.assertEqual(permute_factors(4), 5)
-        self.assertEqual(permute_factors(9), 10)
-        self.assertEqual(permute_factors(999), 1000)
-        self.assertEqual(permute_factors(658), 659)
+    for i in range(size):
+        identity_matrix.append([0] * size)
+        identity_matrix[i][i] = 1
+
+    for identity_row in identity_matrix:
+        _temp = sorted([sum(x) for x in zip(factors, identity_row)])
+        if _temp not in results:
+            results.append(_temp)
+
+    print(results)
+
+    return sorted(results)
+
+
+class TestIncrement(unittest.TestCase):
+    """Test behaviour of increment_factors()"""
 
     def test_list(self):
         """Test behaviour if a list of integers is passed"""
-        self.assertEqual(permute_factors([0, 0, 0, 0]), [0, 0, 0, 1])
-        self.assertEqual(permute_factors([0, 0, 0, 1]), [[0, 0, 1, 1], [0, 0, 0, 2]])
+        self.assertEqual(increment_factors([0, 0, 0, 0]), [[0, 0, 0, 1]])
+        self.assertEqual(increment_factors([0, 1, 1, 1]), [[0, 1, 1, 2], [1, 1, 1, 1]])
+
+
+class TestPermute(unittest.TestCase):
+    """Test behaviour of permute_factor_sets()"""
+
+    def test_none(self):
+        """Test behaviour if nothing is passed"""
+        self.assertIsNone(permute_factor_sets(None))
+
+    def test_integer(self):
+        """Test behaviour if a single integer is passed"""
+        self.assertEqual(permute_factor_sets(4), 5)
+        self.assertEqual(permute_factor_sets(9), 10)
+        self.assertEqual(permute_factor_sets(999), 1000)
+        self.assertEqual(permute_factor_sets(658), 659)
+
+    def test_list(self):
+        """Test behaviour if a list of integers is passed"""
+        self.assertEqual(permute_factor_sets([0, 0]), [[0, 1]])
+        self.assertEqual(permute_factor_sets([0, 1]), [[0, 2], [1, 1]])
+        self.assertEqual(permute_factor_sets([[0, 1]]), [[0, 2], [1, 1]])
+        self.assertEqual(permute_factor_sets([0, 0, 0, 0]), [[0, 0, 0, 1]])
+        self.assertEqual(permute_factor_sets([0, 0, 0, 1]), [[0, 0, 0, 2], [0, 0, 1, 1]])
+        self.assertEqual(permute_factor_sets([0, 0, 1, 1]), [[0, 0, 1, 2], [0, 1, 1, 1]])
+        self.assertEqual(permute_factor_sets([0, 0, 1, 3]), [[0, 0, 1, 4], [0, 0, 2, 3], [0, 1, 1, 3]])
+        self.assertEqual(permute_factor_sets([[0, 0, 0, 4], [0, 0, 1, 3], [0, 1, 1, 2], [1, 1, 1, 1]]),
+                         [[0, 0, 0, 5], [0, 0, 1, 4], [0, 0, 2, 3], [0, 1, 1, 3], [0, 1, 2, 2], [1, 1, 1, 2]])
 
     def test_other(self):
         """Test behaviour if an unintended type is passed"""
-        self.assertRaises(TypeError, permute_factors, "string")
+        self.assertRaises(TypeError, permute_factor_sets, "string")
 
 
 if __name__ == '__main__':
