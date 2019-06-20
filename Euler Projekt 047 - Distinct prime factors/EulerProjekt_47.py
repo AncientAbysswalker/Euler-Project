8eb3636bd6292eca24b79efb0d90053be83fdba6
Ancient Abysswalker <ancientabysswalker@gmail.com>
2019-06-19 17:00:20 -0700

    -Completed. Solution is general, but solution is slow. Tried to shorten time by removing list appending, but this did not speed up the solution much

--- Euler Projekt 047 - Distinct prime factors/EulerProjekt_47.py
+++ Euler Projekt 047 - Distinct prime factors/EulerProjekt_47.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import yaml
 import unittest
 import numpy
 import os
@@ -36,6 +35,23 @@ def recursive_factorization(n, p, index=0):
         return recursive_factorization(n, p, index + 1)
 
 
+def recursive_distinct_factors(n, p, index=0, adder=1):
+    """Generate factors of a number recursively"""
+
+    if n in p:
+        return n != p[index]
+
+    quotient, remainder = n // p[index], n % p[index]
+
+    if remainder == 0:
+        if quotient == 1:
+            return adder
+        else:
+            return adder + recursive_distinct_factors(quotient, p, index, 0)
+    else:
+        return recursive_distinct_factors(n, p, index + 1)
+
+
 def list_to_quantity_dict(factor_list):
     """Convert a list of numbers to a dictionary with numbers as key and count as value"""
     factor_dict = dict()
@@ -44,6 +60,47 @@ def list_to_quantity_dict(factor_list):
         factor_dict[factor] = factor_dict.get(factor, 0) + 1
 
     return factor_dict
+
+
+def consecutive_distinct_prime_factors(n):
+    """Determine the first n consecutive numbers that have n distinct prime factors are"""
+
+    # Load primes and determine smallest number to have n distinct prime factors
+    primes = load_primes()
+    sequential_count = 0
+    current_number = numpy.prod(primes[:n])
+
+    while current_number < primes[-1]:
+        _temp = list_to_quantity_dict(recursive_factorization(current_number, primes))
+        if len(_temp) == n:
+            sequential_count += 1
+        else:
+            sequential_count = 0
+
+        if sequential_count == n:
+            return [current_number - n + i + 1 for i in range(n)]
+
+        current_number += 1
+
+
+def consecutive_distinct_prime_factors2(n):
+    """Determine the first n consecutive numbers that have n distinct prime factors are"""
+
+    # Load primes and determine smallest number to have n distinct prime factors
+    primes = load_primes()
+    sequential_count = 0
+    current_number = numpy.prod(primes[:n])
+
+    while current_number < primes[-1]:
+        if recursive_distinct_factors(current_number, primes) == n:
+            sequential_count += 1
+        else:
+            sequential_count = 0
+
+        if sequential_count == n:
+            return [current_number - n + i + 1 for i in range(n)]
+
+        current_number += 1
 
 
 class TestRecursiveFactors(unittest.TestCase):
@@ -97,10 +154,19 @@ class TestQuantityDict(unittest.TestCase):
         self.assertEqual(list_to_quantity_dict([2, 2, 8867]), {2: 2, 8867: 1})
 
 
-if __name__ == '__main__':
-    unittest.main(exit=False)
-    primes = load_primes()
+class TestConsecutiveDistinct(unittest.TestCase):
+    """Test behaviour of converting a list of numbers to a list of numbers with quantities"""
 
-    print(list_to_quantity_dict(recursive_factorization(4620, primes)))
+    def test_consecutive_distinct_2(self):
+        self.assertEqual(consecutive_distinct_prime_factors(2), [14, 15])
+
+    def test_consecutive_distinct_3(self):
+        self.assertEqual(consecutive_distinct_prime_factors(3), [644, 645, 646])
+
+
+if __name__ == '__main__':
+    # unittest.main(exit=False)
+
+    print(consecutive_distinct_prime_factors2(3))
 
 
